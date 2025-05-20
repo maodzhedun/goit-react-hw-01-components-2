15a13c72edfad665934b2a5b24542608ea6b7e3f
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-05-20 17:15:15 +0300

    01

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import user from '../data/user.json';
+import { Profile } from '../Profile/Profile';
+
+export const App = () => {
+  return (
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </Profile>
+  );
+};
